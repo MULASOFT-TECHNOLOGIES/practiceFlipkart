5a524485ed9ed58137cf87a17737ccaf1d422fce
Mulasoft <info@mulasofttechnologies.com>
2024-08-13 19:22:21 +0530

    some fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { Section1Component } from './components/section1/section1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    Section1Component
   ],
   imports: [
     BrowserModule,
