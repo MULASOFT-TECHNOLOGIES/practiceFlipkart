1b670f7c5b96cf2ad0332f823715f6c0eed4b7d0
swathi <swathi.nallpuram@mulasofttechnologies.cloud>
2024-08-21 18:04:37 +0530

    MPF-1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { Section1Component } from './components/section1/section1.component';
+import { Section2Component } from './components/section2/section2.component';
+import { Section4Component } from './section4/section4.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    Section1Component
+    Section1Component,
+    Section2Component,
+    Section4Component
   ],
   imports: [
     BrowserModule,
